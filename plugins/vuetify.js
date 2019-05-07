975402bd32675c6f1cbc3c22764b3ef5873af0d0
Chris Zubak-Skees <chriszs@gmail.com>
2019-05-07 16:11:34 -0400

    Screw with the colors

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,4 +1,14 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 
-Vue.use(Vuetify);
+// https://github.com/Upstatement/cpi-wordpress/blob/master/static/scss/abstracts/_variables.scss
+
+Vue.use(Vuetify,{
+  theme: {
+    primary: '#04284b', // blue-dark
+    secondary: '#4FA2ff', // blue-bright-2
+    accent: '#0276e8', // blue-bright
+    success: '#1ebcbc', // mint
+    error: '#f35d26' // coral
+  }
+});
